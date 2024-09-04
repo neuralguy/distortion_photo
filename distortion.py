caeb8493716cbbfeb8a52e01dc734233c3c85bae
neuralguy <109704567+neuralguy@users.noreply.github.com>
2024-09-04 16:31:36 +0300

    Update distortion.py

--- distortion.py
+++ distortion.py
@@ -2,99 +2,84 @@ import numpy as np
 from PIL import Image
 
 
-# Path to original image
-path = ""
-# Some parameters
-factor = 100
-frequency = 5
-# Path to output image(optional)
-output_path = ""
-
-
 def quad_as_rect(quad):
-    if quad[0] != quad[2]: return False
-    if quad[1] != quad[7]: return False
-    if quad[4] != quad[6]: return False
-    if quad[3] != quad[5]: return False
-    return True
+    # Проверка, является ли четырёхугольник прямоугольником
+    return quad[0] == quad[2] and quad[1] == quad[7] and quad[4] == quad[6] and quad[3] == quad[5]
 
 
 def quad_to_rect(quad):
-    assert(len(quad) == 8)
-    assert(quad_as_rect(quad))
-    return (quad[0], quad[1], quad[4], quad[3])
+    # Преобразование координат четырёхугольника в координаты прямоугольника
+    if len(quad) != 8 or not quad_as_rect(quad):
+        raise ValueError("Некорректные координаты четырёхугольника")
+    return quad[0], quad[1], quad[4], quad[3]
 
 
 def rect_to_quad(rect):
-    assert(len(rect) == 4)
-    return (rect[0], rect[1], rect[0], rect[3], rect[2], rect[3], rect[2], rect[1])
+    # Преобразование координат прямоугольника в координаты четырёхугольника
+    if len(rect) != 4:
+        raise ValueError("Некорректные координаты прямоугольника")
+    return rect[0], rect[1], rect[0], rect[3], rect[2], rect[3], rect[2], rect[1]
 
 
 def shape_to_rect(shape):
-    assert(len(shape) == 2)
-    return (0, 0, shape[0], shape[1])
+    # Получение координат прямоугольника на основе размеров
+    if len(shape) != 2:
+        raise ValueError("Некорректные размеры")
+    return 0, 0, shape[0], shape[1]
 
 
 def griddify(rect, w_div, h_div):
-    w = rect[2] - rect[0]
-    h = rect[3] - rect[1]
-    x_step = w / float(w_div)
-    y_step = h / float(h_div)
-    y = rect[1]
-    grid_vertex_matrix = []
-    for _ in range(h_div + 1):
-        grid_vertex_matrix.append([])
-        x = rect[0]
-        for _ in range(w_div + 1):
-            grid_vertex_matrix[-1].append([int(x), int(y)])
-            x += x_step
-        y += y_step
-    grid = np.array(grid_vertex_matrix)
-    return grid
+    # Создание сетки на основе размеров прямоугольника
+    x_step = np.linspace(rect[0], rect[2], w_div + 1)
+    y_step = np.linspace(rect[1], rect[3], h_div + 1)
+    return np.dstack(np.meshgrid(x_step, y_step)).astype(int)
 
 
 def distort_grid(org_grid, max_shift):
-    new_grid = np.copy(org_grid)
-    x_min = np.min(new_grid[:, :, 0])
-    y_min = np.min(new_grid[:, :, 1])
-    x_max = np.max(new_grid[:, :, 0])
-    y_max = np.max(new_grid[:, :, 1])
-    new_grid += np.random.randint(- max_shift, max_shift + 1, new_grid.shape)
-    new_grid[:, :, 0] = np.maximum(x_min, new_grid[:, :, 0])
-    new_grid[:, :, 1] = np.maximum(y_min, new_grid[:, :, 1])
-    new_grid[:, :, 0] = np.minimum(x_max, new_grid[:, :, 0])
-    new_grid[:, :, 1] = np.minimum(y_max, new_grid[:, :, 1])
-    return new_grid
+    # Добавление искажений к сетке
+    return np.clip(org_grid + np.random.randint(-max_shift, max_shift + 1, org_grid.shape), np.min(org_grid), np.max(org_grid))
 
 
 def grid_to_mesh(src_grid, dst_grid):
-    assert(src_grid.shape == dst_grid.shape)
+    if src_grid.shape != dst_grid.shape:
+        raise ValueError("Сетки имеют разные размеры")
+    
     mesh = []
     for i in range(src_grid.shape[0] - 1):
         for j in range(src_grid.shape[1] - 1):
-            src_quad = [src_grid[i    , j    , 0], src_grid[i    , j    , 1],
-                        src_grid[i + 1, j    , 0], src_grid[i + 1, j    , 1],
+            src_quad = [src_grid[i, j, 0], src_grid[i, j, 1],
+                        src_grid[i + 1, j, 0], src_grid[i + 1, j, 1],
                         src_grid[i + 1, j + 1, 0], src_grid[i + 1, j + 1, 1],
-                        src_grid[i    , j + 1, 0], src_grid[i    , j + 1, 1]]
-            dst_quad = [dst_grid[i    , j    , 0], dst_grid[i    , j    , 1],
-                        dst_grid[i + 1, j    , 0], dst_grid[i + 1, j    , 1],
+                        src_grid[i, j + 1, 0], src_grid[i, j + 1, 1]]
+            
+            dst_quad = [dst_grid[i, j, 0], dst_grid[i, j, 1],
+                        dst_grid[i + 1, j, 0], dst_grid[i + 1, j, 1],
                         dst_grid[i + 1, j + 1, 0], dst_grid[i + 1, j + 1, 1],
-                        dst_grid[i    , j + 1, 0], dst_grid[i    , j + 1, 1]]
-            dst_rect = quad_to_rect(dst_quad)
-            mesh.append([dst_rect, src_quad])
+                        dst_grid[i, j + 1, 0], dst_grid[i, j + 1, 1]]
+            
+            if quad_as_rect(dst_quad):  # Убедимся, что dst_quad это прямоугольник
+                dst_rect = quad_to_rect(dst_quad)
+                mesh.append([dst_rect, src_quad])
+            else:
+                raise ValueError("Получившийся четырехугольник не является прямоугольником")
     return mesh
 
 
-def main(path, factor, frequency, output):
+def distorize(path, factor=200, frequency=5, output="result.jpg", seed=None):
+    if seed is not None:
+        np.random.seed(seed)
+    # Искажение изображения
     im = Image.open(path)
     dst_grid = griddify(shape_to_rect(im.size), frequency, frequency)
     src_grid = distort_grid(dst_grid, factor)
     mesh = grid_to_mesh(src_grid, dst_grid)
     im = im.transform(im.size, Image.MESH, mesh)
-    if output[-3:] == "jpg" or output[-3:] == "png":
-        im.save(output)
-    else:
-        im.save(f"{output}.jpg")
+    output_path = output or path
+    im.save(output_path)
 
 
-main(path, factor, frequency, output_path if output_path != "" else "./zhmih.jpg")
+if __name__ == "__main__":
+    path = "/home/timon/Изображения/My/kate.jpg"
+    factor = 100
+    frequency = 10
+    distorize(path, factor, frequency, seed=69)
